33c6a3491fffb0f8e946a87a80131834823b0932
Amitkumar Mishra <mr.mash@gmail.com>
2021-09-23 14:25:44 +0530

    removed cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 const app = express()
 app.use(morgan('dev'))
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
