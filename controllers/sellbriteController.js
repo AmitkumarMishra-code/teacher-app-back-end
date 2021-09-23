044096a8dbec9cc05a3fc20672521fa62fa9ba4d
Amitkumar Mishra <mr.mash@gmail.com>
2021-09-23 16:06:53 +0530

    changed to try catch

--- controllers/sellbriteController.js
+++ controllers/sellbriteController.js
@@ -7,45 +7,37 @@ async function getProducts() {
     var config = {
         method: 'get',
         url: 'https://api.sellbrite.com/v1/products',
-        headers: { 
-          'Authorization': 'Basic NTgxODEyYTctZWQ5OS00YmExLTk1ZWItOGM0MzYyZGFlN2E3OmE4MmNjZjA0ZGVmMTVmM2IzYmY5Nzg3N2VmZDE2M2Zk'
+        headers: {
+            'Authorization': 'Basic NTgxODEyYTctZWQ5OS00YmExLTk1ZWItOGM0MzYyZGFlN2E3OmE4MmNjZjA0ZGVmMTVmM2IzYmY5Nzg3N2VmZDE2M2Zk'
         }
-      };
-      
-      axios(config)
-      .then(function (response) {
-          console.log("1")
-        console.log(JSON.stringify(response.data));
-        return {status: true, message : response.data}
-      })
-      .catch(function (error) {
-          console.log("2")
-        console.log(error);
-        return {status: false, message: error.message}
-      });
+    };
+    try {
+        let response = await axios(config)
+        return { status: true, message: response.data }
+    } catch (error) {
+        return { status: false, message: error.message }
+    });
 
 }
 
 async function getInventory() {
+
+    const receivedData = []
+
     var config = {
         method: 'get',
         url: 'https://api.sellbrite.com/v1/inventory',
-        headers: { 
-          'Authorization': 'Basic NTgxODEyYTctZWQ5OS00YmExLTk1ZWItOGM0MzYyZGFlN2E3OmE4MmNjZjA0ZGVmMTVmM2IzYmY5Nzg3N2VmZDE2M2Zk'
+        headers: {
+            'Authorization': 'Basic NTgxODEyYTctZWQ5OS00YmExLTk1ZWItOGM0MzYyZGFlN2E3OmE4MmNjZjA0ZGVmMTVmM2IzYmY5Nzg3N2VmZDE2M2Zk'
         }
-      };
-      
-      axios(config)
-      .then(function (response) {
-          console.log("1")
-        console.log(JSON.stringify(response.data));
-        return {status: true, message : response.data}
-      })
-      .catch(function (error) {
-          console.log("2")
-        console.log(error);
-        return {status: false, message: error.message}
-      });
+    };
+
+    try {
+        let response = await axios(config)
+        return { status: true, message: response.data }
+    } catch (error) {
+        return { status: false, message: error.message }
+    });
 }
 
 module.exports = {
